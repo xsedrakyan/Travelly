2cb8c1e251fd3e1e2450f0865a3ebcd351fb675e
xsedrakyan <khachatur.sedrakyan96@gmail.com>
2020-08-02 23:48:29 +0400

    s This is first commit:

---
+++ Travelly/Travelly/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Travelly
+//
+//  Created by Khachatur Sedrakyan on 8/2/20.
+//  Copyright © 2020 Khachatur Sedrakyan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
